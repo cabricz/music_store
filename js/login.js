5873022a84472d84b307f6804e38194dff9d4984
Zlatko Cabric <cabricz@gmail.com>
2017-01-29 23:27:36 -0500

    login function
    add login function with two hardcoded users
    add my account page

--- js/login.js
+++ js/login.js
@@ -1,18 +1,27 @@
-"use strict";
-
 function validateForm() {
+    "use strict";
+    
     var email = document.getElementById("email");
     var password = document.getElementById("password");
+    
+    if ((email.value === "admin@titanmusicstore.com" && password.value === "LogMeIn") || (email.value === "cabricz@gmail.com" && password.value === "paSSword")) {
+        return true;
+    } else {
+        alert("Incorrect email or password");
+        return false;
+    }
     
     
 }
 
 function init() {
-    if (document && document.getElementById("loginForm") {
-            var loginForm = document.getElementById("loginForm");
-            
-            loginForm.onsubmit = validateForm;
-        }
+    "use strict";
+    
+    if (document && document.getElementById("loginForm")) {
+        var loginForm = document.getElementById("loginForm");
+        
+        loginForm.onsubmit = validateForm;
     }
+}
 
-    window.onload = init;
+window.onload = init;
