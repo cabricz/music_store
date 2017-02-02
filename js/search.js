777f124cac86a89fbb955fa3de97b2b88b54c903
Zlatko Cabric <cabricz@gmail.com>
2017-02-02 00:31:28 -0500

    search function
    add search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,35 @@
 //Search
+
+function displaySearch(result) {
+    "use strict";
+    
+    var displayResult = document.getElementById("displayResult");
+    
+    displayResult.style.display = "block";
+    displayResult.innerHTML = result;
+}
+
+function doSearch() {
+    "use strict";
+    
+    var searchField = document.getElementById("searchField").value;
+    var result = "Nothing found!";
+    
+    for (var i = 0; i < db.length; i++) {
+        if (db[i].artist.toUpperCase() === searchField.toUpperCase()) {
+            result = db[i].id + " - " + db[i].title + " - " + db[i].artist + " - $" + db[i].price + " - " + db[i].relDate;
+        }
+    }
+    
+    displaySearch(result);
+        
+}
+
+function search() {
+    "use strict";
+    
+    var searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener("click", doSearch, false);
+}
+
+window.addEventListener("load", search, false);
